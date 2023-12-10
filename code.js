a718884762d0ce261eb36a2d4b3d1760607c01bc
Andon Mefford <andonmefford@outlook.com>
2023-12-09 21:33:00 -0700

    Update code.js

--- code.js
+++ code.js
@@ -2,17 +2,17 @@ function convertToAdjMatrix(adjList) {
     if(adjList.length < 1) {
         return [];
     }
-else {
-    var adjMatrix = [];
-    for(let i = 0; i < adjList.length; i++) {
-        var edges = [];
-        for(let j = 0; j < adjList.length; j++) {
-            if(adjList[i][j] == 1) {
-                edges.push(j);
+    else {
+        var adjMatrix = [];
+        for(let i = 0; i < adjList.length; i++) {
+            var edges = [];
+            for(let j = 0; j < adjList.length; j++) {
+                if(adjList[i][j] == 1) {
+                    edges.push(j);
+                }
             }
-        }
-        list.push(edges);
-        }
-    return adjMatrix;          
+            adjMatrix.push(edges);
+            }
+        return adjMatrix;          
         }
 }
