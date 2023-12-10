1460b0cd6a231513bfe0a44e2a7a3e1a4033d978
Andon Mefford <andonmefford@outlook.com>
2023-12-09 21:28:07 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,21 +1,18 @@
-// I THINK THIS IS CLOSE TO WHAT I NEED; HOWEVER, THE TESTING SUITE DOES NOT AGREE. I GOT A COUNTEREXAMPLE OF [] MULTIPLE TIMES WHEN ATTEMPTING TO RUN THIS
-
 function convertToAdjMatrix(adjList) {
-        if (adjList.length < 1) {
-                return [];
-        }
-        else {
-        var adjMatirx = [];
-        const maxNode = adjList.length - 1;
-        const adjMatrix = new Array(maxNode + 1).fill(0).map(function () {
-            return new Array(maxNode + 1).fill(0);
-        });
-        for (let i = 0; i <= maxNode; i++) {
-            for (const element of adjList[i]) {
-                const neighbor = element;
-                adjMatrix[i][neighbor] = 1;
+    if(adjList.length < 1) {
+        return [];
+    }
+else {
+    var adjMatrix = [];
+    for(let i = 0; i < adjList.length; i++) {
+        var edges = [];
+        for(let j = 0; j < adjList.length; j++) {
+            if(adjList[i][j] == 1) {
+                edges.push(j);
             }
         }
-    return adjMatrix;
-  }  
+        list.push(edges);
+        }
+    return adjMatrix;          
+        }
 }
